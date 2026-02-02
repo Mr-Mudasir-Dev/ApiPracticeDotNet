62035e90a6c4939dd360d22a768f3e75a5e463c1
Mr-Mudasir-Dev <mudasiraptech@gmail.com>
2026-02-02 06:40:54 -0800

    Connect database and add Students table

---
+++ ApiProject/Model/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.Model
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext>opt)  :base(opt) { }
+        
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- ApiProject/Program.cs
+++ ApiProject/Program.cs
@@ -1,8 +1,15 @@
+using ApiProject.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MyDbContext>(opt => opt.UseSqlServer(
+    builder.Configuration.GetConnectionString("mycon")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
